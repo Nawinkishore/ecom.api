f54df0fc99653c0a162b46c81526ae9a9426bbf0
Nawinkishore <nawinkishore2004@gmail.com>
2024-01-01 17:58:33 +0530

    feat:db

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ import mysql from 'mysql2';
 const conn = mysql.createPool({
     host: '183.83.189.23',
     port: 3306,
-    user: 'fsd',
-    password: 'q2@NF~xCm71IRM$gr7!<lb@P<',
-    database: 'shophub',
+    user: 'nawin',
+    password: 'W#j&QIKttV^pq}ZCyuh6W$b{x',
+    database: 'nawin',
 });
 export default conn;
