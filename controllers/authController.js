a65afb6ef158fc1498f9c35db183921e73aca39e
Nawinkishore <nawinkishore2004@gmail.com>
2024-01-01 19:19:42 +0530

    feat:authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,22 +4,38 @@ export class authController{
     static  register(req, res,){
        let userData = req.body;
     //    console.log(userData);
-    let query = sqlString.format('INSERT INTO User SET?',[userData]);
+    let query = sqlString.format(`SELECT
+        (SELECT COUNT(*) FROM User WHERE email = ?) AS userCountemail,
+        (SELECT COUNT(*) FROM User WHERE phone = ?) AS userCountphone`, [userData.email, userData.phone]);
     conn.query(query,(err,result)=>{
         if(err){
-            console.log(err);
             return res.json({
                 success:false,
-                message:'Error inserting',
+                message:"User unable to login",
             });
-        
         }
-        return res.json({
-            success:true,
-            message:'Success inserting',
+       if(result[0].userCountemail > 0 || result[0].userCountphone >0){
+            return res.json({
+                success:false,
+                message:"User already exists",
+            });
+        }
+    
+        let query = sqlString.format(`insert into User set?`,[userData]);
+        conn.query(query,(err,result)=>{
+            if(err){
+                return res.json({
+                    success:false,
+                    message:"Unable to insert",
+                });
+            }
+            return res.json({
+                success:true,
+                message:"User insert successfully",
+            });
         });
-    })
-        
-
+      
+    });
+ 
     }
 }
