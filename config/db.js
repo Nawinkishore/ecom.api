18be4c4201bfacfa6531f31b7f4bb8b905c6ddcb
Nawinkishore <nawinkishore2004@gmail.com>
2024-01-01 22:59:01 +0530

    chore

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 const conn = mysql.createPool({
-    host: '183.83.189.23',
-    port: 3306,
-    user: 'nawin',
-    password: 'W#j&QIKttV^pq}ZCyuh6W$b{x',
-    database: 'nawin',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 export default conn;
